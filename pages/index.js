a15c6ec86cf0ed83df04f9405219a472f5877d2c
Oluwatobi Omotayo <tobitech@ymail.com>
2021-12-25 18:24:31 +0100

    Fix file important name casing.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
