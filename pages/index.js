de752b41050f287369029aada1cd5b7500635075
Oluwatobi Omotayo <tobitech@ymail.com>
2021-12-25 18:08:09 +0100

    Restore Navigation bar and footer.
    Improve mobile responsiveness of hero image on smaller screens.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Footer from "../components/footer";
 import Hero from "../components/Hero";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
@@ -7,7 +9,9 @@ export default function Home() {
       <Head>
         <title>Ratio App - The most insightful personal finance app</title>
       </Head>
+      <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 }
