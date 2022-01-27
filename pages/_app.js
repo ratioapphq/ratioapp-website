4816a03650d1395a77a09a3e7f281fed79e72a84
Oluwatobi Omotayo <tobitech@ymail.com>
2022-01-28 00:57:11 +0100

    Add a lib folder for 3rd party related work.
    Refactor initial setup with lib functions.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      });
+      ga.pageview(url);
     };
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
     router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
