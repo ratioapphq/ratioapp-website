81a386f795b6caeb2f8f584a425be05328a2f3ec
Oluwatobi Omotayo <tobitech@ymail.com>
2022-04-16 15:01:53 +0100

    Add new features components to display the list of features the mvp would have.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Features from "../components/Features";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
@@ -10,6 +11,7 @@ export default function Home() {
         <title>Ratio App - The most insightful personal finance app</title>
       </Head>
       <Hero />
+      <Features />
     </div>
   );
 }
