7ef14430cdddf2dc94e584630d308a9f43dc42d0
Tobi Omotayo <tobitech@ymail.com>
2023-06-05 14:32:21 +0100

    use container and grid for the hero layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,7 @@ import Link from "next/link";
 
 import clsx from 'clsx'
 import * as ga from "../../lib/ga";
+import { Container } from "@/components/Container";
 import { AppStoreLink } from "@/components/AppStoreLink";
 import { PlayStoreLink } from "@/components/PlayStoreLink";
 import logoBbc from '@/images/logos/bbc.svg'
@@ -25,75 +26,74 @@ export default function Hero() {
   const [alert, setAlert] = useState({ message: "" });
 
   return (
-    <div className="relative bg-white overflow-hidden">
+    <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
       <div
         className="hidden lg:block lg:absolute lg:inset-0"
         aria-hidden="true"
       >
       </div>
-
-      <div className="relative pt-6 sm:pb-24 lg:pb-8">
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
-          <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-            {/* Left Section: Copy */}
-            <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
-              <h1>
-                <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-7xl">
-                  <span className="block text-gray-900">
-                    Build healthier
-                    <br />
-                    financial habits.
-                  </span>
+      <Container>
+        <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
+          {/* Left Section: Copy */}
+          <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
+            <h1>
+              <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-7xl">
+                <span className="block text-gray-900">
+                  Build healthier
+                  <br />
+                  financial habits.
                 </span>
-              </h1>
-              <p className="mt-3 text-base sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                Ratio App lets you see your finances in a whole new light. It analyses your spending patterns to give you personalised insights and tips. Whether you want to pay off debt, save for a vacation, or just live more comfortably, Ratio App helps you make the best financial decisions for your lifestyle.
-              </p>
-              <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
-                <AppStoreLink />
-                <PlayStoreLink />
-              </div>
-              <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
-                <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">
-                  As featured in
-                </p>
-                <ul
-                  role="list"
-                  className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
-                >
-                  {[
-                    ['Forbes', logoForbes],
-                    ['TechCrunch', logoTechcrunch],
-                    ['Wired', logoWired],
-                    ['CNN', logoCnn, 'hidden xl:block'],
-                    ['BBC', logoBbc],
-                    ['CBS', logoCbs],
-                    ['Fast Company', logoFastCompany],
-                    ['HuffPost', logoHuffpost, 'hidden xl:block'],
-                  ].map(([name, logo, className]) => (
-                    <li key={name} className={clsx('flex', className)}>
-                      <Image src={logo} alt={name} className="h-8" unoptimized />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            {/* Right Section: Image */}
-            <div className="relative sm:max-w-lg sm:mx-auto lg:mt-0 mt-12 lg:max-w-none lg:mx-0 lg:col-span-6 flex items-center">
-              {/* iPhone Mockup */}
-              <div className="relative lg:-mt-20 mx-auto lg:max-w-md">
-                <Image
-                  className="mx-auto"
-                  src={heroImage}
-                  alt=""
-                  width={300}
-                  height={614}
-                />
-              </div>
+              </span>
+            </h1>
+            <p className="mt-3 text-base sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+              Ratio App lets you see your finances in a whole new light. It analyses your spending patterns to give you personalised insights and tips. Whether you want to pay off debt, save for a vacation, or just live more comfortably, Ratio App helps you make the best financial decisions for your lifestyle.
+            </p>
+            <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
+              <AppStoreLink />
+              <PlayStoreLink />
             </div>
           </div>
-        </main>
-      </div>
+          {/* Right Section: Image */}
+          <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
+            {/* iPhone Mockup */}
+            {/* <div className="relative lg:-mt-20 mx-auto lg:max-w-md"> */}
+            <div className="-mx-4 h-[448px] px-9 sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
+              <Image
+                className="mx-auto"
+                src={heroImage}
+                alt=""
+                width={300}
+                height={614}
+              />
+            </div>
+          </div>
+          {/* In the press */}
+          <div className="relative -mt-12 lg:col-span-7 lg:mt-0 xl:col-span-6">
+            <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">
+              As featured in
+            </p>
+            <ul
+              role="list"
+              className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
+            >
+              {[
+                ['Forbes', logoForbes],
+                ['TechCrunch', logoTechcrunch],
+                ['Wired', logoWired],
+                ['CNN', logoCnn, 'hidden xl:block'],
+                ['BBC', logoBbc],
+                ['CBS', logoCbs],
+                ['Fast Company', logoFastCompany],
+                ['HuffPost', logoHuffpost, 'hidden xl:block'],
+              ].map(([name, logo, className]) => (
+                <li key={name} className={clsx('flex', className)}>
+                  <Image src={logo} alt={name} className="h-8" unoptimized />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 
