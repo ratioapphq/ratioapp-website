bc5bf9ef25e6f635871ba69551e132574c25b68e
Tobi Omotayo <tobitech@ymail.com>
2023-06-19 17:21:41 +0100

    increase size of download button

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,33 +1,33 @@
 import {
-  SwitchVerticalIcon,
-  SearchIcon,
-  RefreshIcon,
+  ArrowsUpDownIcon,
+  MagnifyingGlassIcon,
+  ArrowPathIcon,
   ChartBarIcon,
   ShieldCheckIcon,
   CurrencyDollarIcon,
-  ViewGridIcon,
-  ZoomInIcon,
+  Squares2X2Icon,
+  MagnifyingGlassPlusIcon,
 } from "@heroicons/react/24/outline";
-import { Container } from "./Container";
+import { Container } from "@/components/Container";
 
 const features = [
   {
     name: "Track expenses & income",
     description:
       "Add rich information to your transactions with notes, transaction type and payment type.",
-    icon: SwitchVerticalIcon,
+    icon: ArrowsUpDownIcon,
   },
   {
     name: "Search your spendings",
     description:
       "Find past purchases and bill payments no matter how long it has been.",
-    icon: SearchIcon,
+    icon: MagnifyingGlassIcon,
   },
   {
     name: "Categorise everything",
     description:
       "Comprehensive list of categories to organise transactions so that you know exactly where your money is going.",
-    icon: ViewGridIcon,
+    icon: Squares2X2Icon,
   },
   {
     name: "Detailed Insights",
@@ -39,7 +39,7 @@ const features = [
     name: "Syncing",
     description:
       "Sync data across multiple devices. Your data is securely backed up in the cloud.",
-    icon: RefreshIcon,
+    icon: ArrowPathIcon,
   },
   {
     name: "Local currencies",
@@ -55,20 +55,20 @@ const features = [
   {
     name: "Accessibility",
     description: "Easy on the eyes with bold text and dark mode support.",
-    icon: ZoomInIcon,
+    icon: MagnifyingGlassPlusIcon,
   },
 ];
 
 export default function Features() {
   return (
-    <div className="bg-white">
+    <div className="bg-white border-t border-gray-200">
         <Container>
       <div className="max-w-4xl mx-auto py-16 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24">
         <h2 className="text-5xl font-extrabold text-gray-900 tracking-tight">
             <span>
                 Designed to help you
                 <br />
-                become financially responsible
+                become financially responsible.
             </span>
         </h2>
         <p className="mt-4 max-w-3xl text-lg text-gray-600">
@@ -80,10 +80,10 @@ export default function Features() {
             <div key={feature.name}>
               <div>
                 <span className="flex items-center justify-center h-12 w-12 rounded-full bg-ratiogreen-100 bg-opacity-30">
-                  {/* <feature.icon
-                    className="h-6 w-6 text-ratiogreen-500"
+                  <feature.icon
+                    className="h-6 w-6 text-ratiogreen-700"
                     aria-hidden="true"
-                  /> */}
+                  />
                 </span>
               </div>
               <div className="mt-6">
