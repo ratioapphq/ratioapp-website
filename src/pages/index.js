963703b162acede530334caa4b39753baf80dc89
Tobi Omotayo <tobitech@ymail.com>
2023-06-19 16:59:04 +0100

    add features component to site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Hero from "@/components/Hero";
 import FAQs from "@/components/FAQs";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <title>Ratio App - The most insightful personal finance app</title>
       </Head>
       <Hero />
+      <Features />
       <FAQs />
       {/* Another newsletter component here for them to sign up for updates. */}
     </div>
