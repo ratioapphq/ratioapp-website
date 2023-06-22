cdb82b9c22a39499510f37d42811303dca38ccde
Tobi Omotayo <tobitech@ymail.com>
2023-06-22 18:50:53 +0100

    code formatting

--- src/components/NewsletterForm.js
+++ src/components/NewsletterForm.js
@@ -1,41 +1,36 @@
+import Script from "next/script"
+
 export function NewsletterForm() {
   return (
-    <div className="bg-white py-16 sm:py-24 lg:py-32">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
-        <div className="max-w-xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7">
-          <h2 className="inline sm:block lg:inline xl:block">Want product news and updates?</h2>{' '}
-          <p className="inline sm:block lg:inline xl:block">Sign up for our newsletter.</p>
-        </div>
-        <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
-          <div className="flex gap-x-4">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <button
-              type="submit"
-              className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Subscribe
-            </button>
-          </div>
-          <p className="mt-4 text-sm leading-6 text-gray-900">
-            We care about your data. Read our{' '}
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-              privacy&nbsp;policy
-            </a>
-            .
-          </p>
-        </form>
+    <div className="border-t border-gray-200 pt-8 lg:flex lg:items-center lg:justify-between py-16">
+      <div>
+        <h3 className="text-sm font-semibold leading-6 text-gray-900">Subscribe to our newsletter</h3>
+        <p className="mt-2 text-sm leading-6 text-gray-600">
+          Product announcements, feature updates, user stories and personal finance tips.
+        </p>
       </div>
+      <form className="mt-6 sm:flex sm:max-w-md lg:mt-0">
+        <label htmlFor="email-address" className="sr-only">
+          Email address
+        </label>
+        <input
+          type="email"
+          name="email-address"
+          id="email-address"
+          autoComplete="email"
+          required
+          className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:w-56 sm:text-sm sm:leading-6"
+          placeholder="Enter your email"
+        />
+        <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+          <button
+            type="submit"
+            className="flex w-full items-center justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+          >
+            Subscribe
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
